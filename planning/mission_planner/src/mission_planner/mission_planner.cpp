159313d56881a67ce0dc5ae54373422327fd79a5
weiwei.he <weiwei.he@upower.com>
2023-04-28 18:08:36 +0800

    add macro gard for switching header name on galacitc.

--- planning/mission_planner/src/mission_planner/mission_planner.cpp
+++ planning/mission_planner/src/mission_planner/mission_planner.cpp
@@ -16,7 +16,11 @@
 
 #include <autoware_adapi_v1_msgs/srv/set_route.hpp>
 #include <autoware_adapi_v1_msgs/srv/set_route_points.hpp>
+#ifdef ROS_DISTRO_GALACTIC
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#else
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#endif
 
 #include <array>
 #include <random>
