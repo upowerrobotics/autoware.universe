849791a556ef7ac719fcb3c208892378625abbb7
CharlesXu1124 <xuzheyuan961124@gmail.com>
2023-06-23 12:20:46 -0700

    removed unused dependency

--- perception/euclidean_cluster/src/euclidean_cluster_node.hpp
+++ perception/euclidean_cluster/src/euclidean_cluster_node.hpp
@@ -20,7 +20,6 @@
 
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
-#include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
 
 #include <memory>
 
