0ba7061de630957d180f628fd81076d79fb73ff2
Stephen Zeng <chuangshuo.zeng@upower.com>
2023-07-12 16:25:53 +0800

    Add python launch files for mission planning.

---
+++ planning/mission_planner/launch/goal_pose_visualizer.launch.py
@@ -0,0 +1,48 @@
+# Copyright 2023 U Power Robotics, USA
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+
+    # arguments
+    route_topic_name_arg = DeclareLaunchArgument(
+        name="route_topic_name",
+        default_value="/planning/mission_planning/route",
+    )
+
+    echo_back_goal_pose_topic_name_arg = DeclareLaunchArgument(
+        name="echo_back_goal_pose_topic_name",
+        default_value="/planning/mission_planning/echo_back_goal_pose",
+    )
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="False",
+        description="Use simulated time",
+    )
+
+    goal_pose_visualizer_node = Node(
+        package="mission_planner",
+        executable="goal_pose_visualizer",
+        name="goal_pose_visualizer",
+        output="screen",
+        remappings=[
+            ("input/route", LaunchConfiguration("route_topic_name")),
+            ("output/goal_pose", LaunchConfiguration("echo_back_goal_pose_topic_name")),
+        ],
+        parameters=[
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ]
+    )
+
+
+    return LaunchDescription([
+        route_topic_name_arg,
+        echo_back_goal_pose_topic_name_arg,
+        use_sim_time_arg,
+        goal_pose_visualizer_node
+    ])
---
+++ planning/mission_planner/launch/mission_planner.launch.py
@@ -0,0 +1,66 @@
+# Copyright 2023 U Power Robotics, USA
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+
+    mission_planner_share_path = get_package_share_directory("mission_planner")
+
+    # arguments
+    modified_goal_topic_name_arg = DeclareLaunchArgument(
+        name="modified_goal_topic_name",
+        default_value="/planning/scenario_planning/modified_goal",
+    )
+
+    map_topic_name_arg = DeclareLaunchArgument(
+        name="map_topic_name",
+        default_value="/map/vector_map",
+        description="lanelet2 map topic name",
+    )
+
+    visualization_topic_name_arg = DeclareLaunchArgument(
+        name="visualization_topic_name",
+        default_value="/planning/mission_planning/route_marker",
+    )
+
+    mission_planner_param_path_arg = DeclareLaunchArgument(
+        name="mission_planner_param_path",
+        default_value=os.path.join(mission_planner_share_path, "config/mission_planner.param.yaml"),
+    )
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="False",
+        description="Use simulated time",
+    )
+
+    mission_planner_node = Node(
+        package="mission_planner",
+        executable="mission_planner",
+        name="mission_planner",
+        output="screen",
+        remappings=[
+            ("input/modified_goal", LaunchConfiguration("modified_goal_topic_name")),
+            ("input/vector_map", LaunchConfiguration("map_topic_name")),
+            ("debug/route_marker", LaunchConfiguration("visualization_topic_name")),
+        ],
+        parameters=[
+            LaunchConfiguration("mission_planner_param_path"),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
+    )
+
+    return LaunchDescription([
+        modified_goal_topic_name_arg,
+        map_topic_name_arg,
+        visualization_topic_name_arg,
+        mission_planner_param_path_arg,
+        use_sim_time_arg,
+        mission_planner_node
+    ])
